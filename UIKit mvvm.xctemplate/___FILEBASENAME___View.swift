843b2bfb486318d3bdf25a064527ee33550b4d66
Manish Pandey <50369307+manishpandey-git@users.noreply.github.com>
2020-09-12 03:45:11 +0530

    Added UIkit mvvm file temp.

---
+++ UIKit mvvm.xctemplate/___FILEBASENAME___View.swift
@@ -0,0 +1,20 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___: UIViewController {
+
+    private let viewModel: ___VARIABLE_productName___ViewModel
+
+    init (viewModel: ___VARIABLE_productName___ViewModel) {
+        self.viewModel = viewModel
+
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+}
+
---
+++ UIKit mvvm.xctemplate/___FILEBASENAME___ViewModel.swift
@@ -0,0 +1,15 @@
+//  ___FILEHEADER___
+
+import UIKit
+
+class ___FILEBASENAME___ {
+
+    private(set) var data: ___VARIABLE_productName___
+
+    init(_ data: ___VARIABLE_productName___) {
+        self.data = data
+    }
+    
+
+}
+
